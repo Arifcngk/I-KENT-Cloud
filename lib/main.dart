dfa2e093e29a4c15d60cac68defb5672f78dde13
Arifcngk <93188117+Arifcngk@users.noreply.github.com>
2024-04-03 01:29:47 +0300

    Added FlutterFire links

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:e_belediyecilik/firebase_options.dart';
 import 'package:e_belediyecilik/screens/login_page.dart';
 import 'package:e_belediyecilik/screens/welcome_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,15 +1,133 @@
+import 'package:e_belediyecilik/misc/colors.dart';
+import 'package:e_belediyecilik/screens/home_page.dart';
+import 'package:e_belediyecilik/screens/signup_page.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key});
 
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: SingleChildScrollView(
+            child: Container(
+              margin: const EdgeInsets.all(24),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _logo(context),
+                  _inputField(context),
+                  _forgotPassword(context),
+                  _signup(context),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _logo(BuildContext context) {
+    return Container(
+      child: Image.asset("img/logo.png"),
+    );
+  }
+
+  Widget _header(BuildContext context) {
+    return const Column(
+      children: [
+        Text(
+          "E-Belediyecilik",
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+        Text("Devam Etmek için Lütfen Giriş Yapınız"),
+      ],
+    );
+  }
+
+  Widget _inputField(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          decoration: InputDecoration(
+              hintText: "Kullanıcı Adı:",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(18),
+                  borderSide: BorderSide.none),
+              fillColor: Colors.blueGrey.withOpacity(0.1),
+              filled: true,
+              prefixIcon: const Icon(Icons.person)),
+        ),
+        const SizedBox(height: 10),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Parola",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide.none),
+            fillColor: Colors.blueGrey.withOpacity(0.1),
+            filled: true,
+            prefixIcon: const Icon(Icons.password),
+          ),
+          obscureText: true,
+        ),
+        const SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      HomePage()), // Burada DashboardPage'e geçiş yapılacak sayfayı belirtmeniz gerekiyor
+            );
+          },
+          style: ElevatedButton.styleFrom(
+            shape: const StadiumBorder(),
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            backgroundColor: AppColors.blueColor,
+          ),
+          child: const Text(
+            "Giriş",
+            style: TextStyle(fontSize: 20),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _forgotPassword(BuildContext context) {
+    return TextButton(
+      onPressed: () {},
+      child: const Text(
+        "Şifremi Unuttum?",
+        style: TextStyle(color: Colors.black),
+      ),
+    );
+  }
+
+  Widget _signup(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Hesabın Yok mu? "),
+        TextButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SignupPage(),
+                  ));
+            },
+            child: const Text(
+              "Kayıt Ol",
+              style: TextStyle(color: Colors.black),
+            ))
+      ],
+    );
   }
 }
